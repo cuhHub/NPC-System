614454c4adbac372cf2d3acd496ab43ac50b72e7
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-01-04 20:06:56 +0000

    internal functions

---
+++ src/p3_library/NPC/internal.lua
@@ -0,0 +1,36 @@
+--------------------------------------------------------
+-- [NPC Dialog Demo] NPC Library - Internal
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    INFO:
+        Contains internal library functions n stuff.
+
+    CREDIT:
+        Author: @cuh6_ (Discord)
+        GitHub Repository: https://github.com/Cuh4/NPCDialogDemo
+        Created: 04/01/2024
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Main
+-------------------------------
+-- Returns whether or not two strings are identical
+---@param str1 string
+---@param str2 string
+NPCLibrary.internal.isIdentical = function(str1, str2)
+    local newStr1 = NPCLibrary.internal.stripPunctuation(str1:lower())
+    local newStr2 = NPCLibrary.internal.stripPunctuation(str2:lower())
+
+    return newStr1:find(newStr2) or newStr2:find(newStr1)
+end
+
+-- Strip punctuation
+---@param input string
+NPCLibrary.internal.stripPunctuation = function(input)
+    return input:gsub("%p", "")
+end
